688d9a76adb029c31ab67553536fee5a6b934dc8
MohamedHashim <sawmido2000@gmail.com>
2021-01-17 00:03:52 +0200

    Push Soldier and Bananas - A solution

--- problems/codeforces/Soldier_and_Bananas_A.cpp
+++ problems/codeforces/Soldier_and_Bananas_A.cpp
@@ -1,25 +1,25 @@
-////
-//// Created by Mohamed Hashim on 1/9/2021.
-////
-//#include"bits/stdc++.h"
-//#include<iostream>
 //
-//using namespace std;
+// Created by Mohamed Hashim on 1/9/2021.
 //
-//int main() {
-//#ifndef ONLINE_JUDGE
-//    FILE *stream;
-//    freopen_s(&stream, "..//input.in", "r", stdin);
-//#endif
-//    int k, n, w, cost = 0;
-//    cin >> k >> n >> w;
-//    for (int i = 1; i <= w; i++) {
-//        cost += i * k;
-//    }
-//    if (n<cost)
-//        cout << abs(cost - n) << endl;
-//    else
-//        cout << 0 << endl;
-//    return 0;
-//}
-//
+#include"bits/stdc++.h"
+#include<iostream>
+
+using namespace std;
+
+int main() {
+#ifndef ONLINE_JUDGE
+    FILE *stream;
+    freopen_s(&stream, "..//input.in", "r", stdin);
+#endif
+    int k, n, w, cost = 0;
+    cin >> k >> n >> w;
+    for (int i = 1; i <= w; i++) {
+        cost += i * k;
+    }
+    if (n<cost)
+        cout << abs(cost - n) << endl;
+    else
+        cout << 0 << endl;
+    return 0;
+}
+
