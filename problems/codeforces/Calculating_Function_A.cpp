bceb78644be6a679070014811e1d0c6a4bd7b383
Mohamed Hashim <sawmido2000@gmail.com>
2021-03-07 02:12:43 +0200

    Add better solution for Calculating Function problem

--- problems/codeforces/Calculating_Function_A.cpp
+++ problems/codeforces/Calculating_Function_A.cpp
@@ -5,20 +5,17 @@ using namespace std;
 
 const int N = 1e5 + 7;
 
-
 int main() {
 #ifndef ONLINE_JUDGE
     FILE *stream;
     freopen_s(&stream, "..//input.in", "r", stdin);
 #endif
 
-    int n, cnt=0;
-    cin>>n;
-    for(int i=1;i<=n;i++){
-        if(i%2!=0)
-            i=i*-1;
-        cnt+=i;
-    }
-    cout<<cnt;
+    long long n;
+    cin >> n;
+    if (n % 2 == 0)
+        cout << n / 2;
+    else
+        cout << -(n+1) / 2;
     return 0;
 }
