0d3bf6827d56d1af17aeb01d36290a9615ff67b1
Mohamed Hashim <mohamedhashim28@gmail.com>
2021-03-28 23:39:16 +0200

    Another TL sol for word subsets

--- problems/leetcode/916_word_subsets.cpp
+++ problems/leetcode/916_word_subsets.cpp
@@ -1,5 +1,22 @@
 class Solution {
 public:
+    bool isContain(string s1, string s2){
+	int cnt =0;
+	for(int i=0;i<s2.size();i++){
+		for(int j=0;j<s1.size();j++){
+			if(s2[i]==s1[j]){
+                s1[j]='0';
+				cnt++;
+				break;
+			}
+		}
+	}
+	if(cnt==s2.size())
+		return true;
+	else
+		return false;
+}
+    
     vector<string> wordSubsets(vector<string>& A, vector<string>& B) {
         vector<string> res;
         for(int i=0;i< A.size();i++){
@@ -7,8 +24,10 @@ public:
         }
         for(int i =0;i<B.size();i++){
             for(int j=0;j<res.size();j++){
-                if(res[j].find(B[i]) == std::string::npos)
+                if(!isContain(res[j],B[i])){
                    res.erase(find(res.begin(),res.end(),res[j]));
+                   j--;
+                }
             }
         }
         return res;
